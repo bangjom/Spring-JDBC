993900b73a6a497a3229fcc23cf99135682c8233
kbj7227@naver.com <bjkim@bjkim.localdomain>
2021-01-04 17:40:56 +0900

    feat(test) : use spring test annotation

---
+++ src/test/java/kr/or/connect/DataSourceTest.java
@@ -0,0 +1,27 @@
+package kr.or.connect;
+
+import java.sql.Connection;
+import javax.sql.DataSource;
+import kr.or.connect.config.ApplicationConfig;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {ApplicationConfig.class})
+public class DataSourceTest {
+    @Autowired
+    DataSource dataSource;
+
+    @Test
+    public void connectTest() throws Exception{
+        //when
+        Connection conn = dataSource.getConnection();
+
+        //then
+        Assert.assertNotNull(conn);
+    }
+}
